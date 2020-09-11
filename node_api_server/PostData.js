aec2028dc0b82c8c1b10810955f4c218e46515c0
Joyceline1 <sweetjoy.ja59@gmail.com>
2020-09-10 17:30:12 -0700

    Mongo Assignment

---
+++ node_api_server/PostData.js
@@ -0,0 +1,20 @@
+let MongoClient = require('mongodb').MongoClient;
+let url = "mongodb://localhost:27017/";
+     
+    const csvFilePath='covid19data.csv';
+    const csv=require('csvtojson');
+     
+     csv()
+    .fromFile(csvFilePath)
+    .then((jsonObj)=>{
+        console.log(jsonObj);
+      MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true }, (err, db) => {
+        if (err) throw err;
+        var dbo = db.db("covidapi");
+        dbo.collection("covid19data").insertMany(jsonObj, (err, res) => {
+        if (err) throw err;
+        console.log("Number of documents inserted: " + res.insertedCount);
+        db.close();
+        });
+      });
+    })
